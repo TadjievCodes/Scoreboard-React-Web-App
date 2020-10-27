49a894f2c75986d701731096c8302e04cd349b2d
TadjievCodes <muhammad2008.08@mail.ru>
2020-10-28 04:25:29 +0500

    added id into players array of

--- 3 - Introducing Props/1 - Setting and Using Props/scoreboard/app.js
+++ 3 - Introducing Props/1 - Setting and Using Props/scoreboard/app.js
@@ -2,19 +2,23 @@
 const players = [ 
    {
     name: "Guil",
-    score: 50
+    score: 50,
+    id: 1
   },
   {
     name: "Treasure",
-    score: 85
+    score: 85,
+    id: 2
   },
   {
     name: "Ashley",
-    score: 95
+    score: 95,
+    id: 3
   },
   {
     name: "James",
-    score: 80
+    score: 80,
+    id: 4
   }
 ];
 
