80bf2b434241a9b97103cb065993411326ca0e79
TadjievCodes <muhammad2008.08@mail.ru>
2020-10-30 05:12:46 +0500

    class Counter extends React.Component

--- 3 - Introducing Props/1 - Setting and Using Props/scoreboard/app.js
+++ 3 - Introducing Props/1 - Setting and Using Props/scoreboard/app.js
@@ -46,14 +46,16 @@ const Player = (props) => {
   );
 }
 
-const Counter = (props) => {
-  return (
+class Counter extends React.Component {
+  render(){
+    return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
+      <span className="counter-score">{this.props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
-  );
+   );
+  }
 }
 
 const App = (props) => {
